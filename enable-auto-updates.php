d0765b3b577d6a288e1a6dbcb5efd07051bec402
sc-pulsion <stewart.campbell@pulsion.co.uk>
2016-07-14 09:26:24 +0100

    Added some more filters
    Make sure the defaults are set.
    Enable the update debug notification emails.
    Bump the version up.

--- enable-auto-updates.php
+++ enable-auto-updates.php
@@ -3,14 +3,41 @@
 Plugin Name: Enable Auto Updates
 Plugin URI: https://github.com/sc-pulsion/wp_auto_update_mu_plugin/
 Description: Enable auto updates for all plugins and themes
-Version: 0.1
+Version: 0.2
 Author: Stewart Campbell
 Author URI: http://www.pulsion.co.uk/
 License: GPL
 Copyright: Pulsion Technology Ltd
 */
 
+/* 
+ * Enable updates even if version control is detected
+ */
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_false' );
+
+/*
+ * Make sure we are only updating minor versions
+ */
+add_filter( 'allow_dev_auto_core_updates', '__return_false' );
+add_filter( 'allow_major_auto_core_updates', '__return_false' );
+add_filter( 'allow_minor_auto_core_updates', '__return_true' );
+
+/* 
+ * Enable plugin and theme updates 
+ */
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
+
+/* 
+ * Translations should be enabled by default but make sure
+ */
+add_filter( 'auto_update_translation', '__return_true' );
+
+/* 
+ * Make sure all email notifications are turned on 
+ * We may want to set the debug filter to false if the notifications get annoying 
+ */
+add_filter( 'auto_core_update_send_email', '__return_true' );
+add_filter( 'automatic_updates_send_debug_email', '__return_true' );
 
 ?>
